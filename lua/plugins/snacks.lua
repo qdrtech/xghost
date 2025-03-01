cfa9286c24f59451c4b8aa58874e794bf1dc5855
qdrtech <qdr.dev@gmail.com>
2025-02-28 23:32:40 -0800

    cleanup configs

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,12 +1,12 @@
 return {
-    "folke/snacks.nvim",
-    opts = {
-        dashboard = {
-            preset = {
-                pick = function(cmd, opts)
-                    return LazyVim.pick(cmd, opts)()
-                end,
-                header = [[
+	"folke/snacks.nvim",
+	opts = {
+		dashboard = {
+			preset = {
+				pick = function(cmd, opts)
+					return LazyVim.pick(cmd, opts)()
+				end,
+				header = [[
         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
@@ -14,21 +14,27 @@ return {
         ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
  ]],
-            }
-        },
-      notifier = { enabled = true },
-  
-      -- show hidden files in snacks.explorer
-      picker = {
-        sources = {
-          explorer = {
-            -- show hidden files like .env
-            hidden = true,
-            -- show files ignored by git like node_modules
-            ignored = true,
-            exclude = { "node_modules", ".git" },
-          },
-        },
-      },
-  },
-  }
+			},
+		},
+		notifier = { enabled = true },
+
+		-- show hidden files in snacks.explorer
+		picker = {
+			sources = {
+				explorer = {
+					-- show hidden files like .env
+					hidden = true,
+					-- show files ignored by git like node_modules
+					ignored = true,
+					exclude = { "node_modules", ".git" },
+					win = {
+						list = {
+							keys = {},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
