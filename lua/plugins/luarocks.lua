1fc7a9398db25e8a9b9705aeb86d4dca8d3a6f0f
qdrtech <qdr.dev@gmail.com>
2026-07-16 11:11:35 -0700

    fix(nvim): remove unused, misconfigured luarocks.nvim
    Its main module is luarocks-nvim, but the spec used config = true, so lazy
    called require('luarocks').setup() and errored on load. Nothing in the config
    depends on it and it needs system luarocks/hererocks that aren't installed.

--- lua/plugins/luarocks.lua
+++
@@ -1,7 +0,0 @@
-local M = {
-	"vhyrro/luarocks.nvim",
-	priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-	config = true,
-}
-
-return M
